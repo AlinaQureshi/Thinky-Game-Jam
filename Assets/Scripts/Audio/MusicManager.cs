9e3fbcac9c75b926a9613bf16f3c91d743bb18b8
Will <55513273+Whullum@users.noreply.github.com>
2023-12-17 13:46:44 -0500

    added stinger music and cleaned up music playback/stopping code

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -1,18 +1,21 @@
+using FMOD.Studio;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    const string masterBusPath = "bus:/";
-    const string sfxBusPathl = "bus:/SFX";
-    const string musicBusPath = "bus:/Music";
-
     const string musicMenuPath = "event:/Frame_of_Mind_Menu";
+    const string musicStingerPath = "event:/Frame_of_Mind_Stinger";
     const string musicGamePath = "event:/Frame_of_Mind";
 
     private FMOD.Studio.EventInstance musicGameEvent;
     private FMOD.Studio.EventInstance musicMenuEvent;
+    private FMOD.Studio.EventInstance musicStingerEvent;
+
+    public EventInstance MusicGameEvent { get => musicGameEvent; }
+    public EventInstance MusicMenuEvent { get => musicMenuEvent; }
+    public EventInstance MusicStingerEvent { get => musicStingerEvent; }
 
     // Start is called before the first frame update
     void Start()
@@ -32,15 +35,17 @@ public class MusicManager : MonoBehaviour
         musicMenuEvent.start();
     }
 
-    public void StopGameplayMusic()
+    private void PlayStingerMusic()
     {
-        musicGameEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        musicGameEvent.release();
+        musicStingerEvent = FMODUnity.RuntimeManager.CreateInstance(musicStingerPath);
+        musicStingerEvent.start();
     }
 
-    public void StopMainMenuMusic()
+    public void StopMusic(FMOD.Studio.EventInstance musicEvent) 
     {
-        musicMenuEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        musicMenuEvent.release();
+        if (!musicEvent.isValid()) return;
+
+        musicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        musicEvent.release();
     }
 }
